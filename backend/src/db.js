1fbc12804dba214cda61b15728a387369c62b3ed
Gustavo Amaro <gustavo.amaro97@gmail.com>
2020-10-12 13:46:22 -0300

    Cria rotas de crud da aplicação

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,28 @@
 const mongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
 
 function findAll(callback){  
     global.conn.collection("products").find({}).toArray(callback);
+}
+
+function findOne(id, callback){  
+    global.conn.collection("products").find(new ObjectId(id)).toArray(callback);
+}
+
+function insert(product, callback){
+    global.conn.collection('products').insertOne(product, callback);
+}
+
+function update(id, product, callback){
+    global.conn.collection("products").updateOne({_id: new ObjectId(id)}, {$set: product}, callback);
+}
+
+function deleteOne(id, callback){
+    global.conn.collection("products").deleteOne({_id: new ObjectId(id)}, callback);
 }
 
 mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true })
             .then(conn => global.conn = conn.db("test"))
             .catch(err => console.log(err))
  
-module.exports = { findAll }
+module.exports = { findAll, insert, findOne, update, deleteOne }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,47 @@ const routes = express.Router();
 routes.get('/products', (req, res)=>{
     global.db.findAll((e, docs) => {
         if(e) { return console.log(e); }
-        res.json({ title: 'Lista de Clientes', docs: docs });
+        res.json({ products: docs });
     })
 });
+
+routes.get('/products/:id', (req, res)=>{
+    const id = req.params.id;
+    global.db.findOne(id, (e, products)=>{
+        if(e) return console.log(e);
+
+        res.json({ product: products[0] });
+    })
+});
+
+routes.post('/products', (req, res)=>{
+    const product = req.body.product;
+    global.db.insert(product, (e, result)=>{
+        if(e) return console.log(e);
+        res.json({ result });
+    })
+});
+
+routes.put('/products/:id', (req, res)=>{
+    const product = req.body.product;
+    const id = req.params.id;
+
+    global.db.update(id, product, (e, result)=>{
+        if(e) return console.log(e);
+
+        res.json({ result });
+    })
+});
+
+routes.delete('/products/:id', (req, res)=>{
+    const id = req.params.id;
+
+    global.db.deleteOne(id, (e, result)=>{
+        if(e) return res.json({error: e});
+
+        res.json({ result })
+    })
+})
 
 
 module.exports = routes;
